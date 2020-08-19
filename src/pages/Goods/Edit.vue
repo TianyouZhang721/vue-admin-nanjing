5637ab6e43ad0da1734e2969d1136fc7e3a60ea7
TianyouZhang721 <zhangty_0721@163.com>
2020-08-19 11:45:24 +0800

    一级分类耳机分类

--- src/pages/Goods/Edit.vue
+++ src/pages/Goods/Edit.vue
@@ -7,31 +7,30 @@
     </el-breadcrumb>
 
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="一级分类" prop="pid">
-        <el-select v-model="ruleForm.pid" placeholder="请选择上级">
+      <el-form-item label="一级分类" prop="first_cateid">
+        <el-select @change="firstCateChange" v-model="ruleForm.first_cateid" placeholder="请选择上级">
+          <el-option v-for="(item, index) in firstCate" :key="index" :label="item.catename" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+
+
+      <el-form-item label="二级分类" prop="second_cateid">
+        <el-select v-model="ruleForm.second_cateid" placeholder="请选择上级">
           <el-option label="顶级菜单" value="0"></el-option>
           <el-option v-for="(item, index) in menuList" :key="index" :label="item.title" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
 
 
-      <el-form-item label="二级分类" prop="pid">
-        <el-select v-model="ruleForm.pid" placeholder="请选择上级">
-          <el-option label="顶级菜单" value="0"></el-option>
-          <el-option v-for="(item, index) in menuList" :key="index" :label="item.title" :value="item.id"></el-option>
-        </el-select>
-      </el-form-item>
 
-
-
-      <el-form-item label="商品名称" prop="rolename">
-        <el-input v-model="ruleForm.rolename"></el-input>
+      <el-form-item label="商品名称" prop="goodsname">
+        <el-input v-model="ruleForm.goodsname"></el-input>
       </el-form-item>
-      <el-form-item label="商品价格" prop="rolename">
-        <el-input v-model="ruleForm.rolename"></el-input>
+      <el-form-item label="商品价格" prop="price">
+        <el-input v-model="ruleForm.price"></el-input>
       </el-form-item>
-      <el-form-item label="市场价格" prop="rolename">
-        <el-input v-model="ruleForm.rolename"></el-input>
+      <el-form-item label="市场价格" prop="market_price">
+        <el-input v-model="ruleForm.market_price"></el-input>
       </el-form-item>
 
 
@@ -49,29 +48,29 @@
       </el-form-item>
 
 
-      <el-form-item label="商品规格" prop="pid">
-        <el-select v-model="ruleForm.pid" placeholder="请选择上级">
+      <el-form-item label="商品规格" prop="specsid">
+        <el-select v-model="ruleForm.specsid" placeholder="请选择上级">
           <el-option label="顶级菜单" value="0"></el-option>
           <el-option v-for="(item, index) in menuList" :key="index" :label="item.title" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
 
-      <el-form-item label="规格属性" prop="pid">
-        <el-select v-model="ruleForm.pid" placeholder="请选择上级">
+      <el-form-item label="规格属性" prop="specsattr">
+        <el-select v-model="ruleForm.specsattr" placeholder="请选择上级">
           <el-option label="顶级菜单" value="0"></el-option>
           <el-option v-for="(item, index) in menuList" :key="index" :label="item.title" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
 
-      <el-form-item label="是否热卖" prop="type">
-        <el-radio-group v-model="ruleForm.type">
+      <el-form-item label="是否热卖" prop="ishot">
+        <el-radio-group v-model="ruleForm.ishot">
           <el-radio label="1">目录</el-radio>
           <el-radio label="2">菜单</el-radio>
         </el-radio-group>
       </el-form-item>
 
-      <el-form-item label="是否新品" prop="type">
-        <el-radio-group v-model="ruleForm.type">
+      <el-form-item label="是否新品" prop="isnew">
+        <el-radio-group v-model="ruleForm.isnew">
           <el-radio label="1">目录</el-radio>
           <el-radio label="2">菜单</el-radio>
         </el-radio-group>
@@ -82,7 +81,7 @@
       <el-form-item label="状态" prop="status">
         <el-switch v-model="ruleForm.status"></el-switch>
       </el-form-item>
-      
+
       <el-form-item>
         <div id="editor"></div>
       </el-form-item>
@@ -107,9 +106,17 @@
         title: "",
         menuList: [],
         ruleForm: {
-          rolename: "",
-          menus: [],
-          status: false,
+          first_cateid: "",
+          second_cateid: "",
+          goodsname:"",
+          price: "",
+          market_price: "",
+          specsid: "",
+          specattr: "",
+          isnew: "",
+          ishot: "",
+          status: true,
+          description: ""
         },
         id: "",
         rules: {
@@ -119,12 +126,20 @@
             trigger: 'blur'
           }, ]
         },
-        editor: null
+        editor: null,
+        firstCate: []
       };
     },
     // created() {},
     mounted() {
+      // 获取一级分类
+      this.$http.get("/catelist",{pid: 0}).then(res => {
+        console.log(res)
+        this.firstCate = res.data.list
+      })
+      // 获取路由参数
       this.id = this.$route.params.id;
+      // 生成富文本编辑器实例
       this.editor = new wangEditor("#editor")
       this.editor.create()
       if (this.id) {
@@ -154,6 +169,12 @@
       })
     },
     methods: {
+      firstCateChange(id) {
+        console.log(id)
+        this.$http.get("/catelist",{pid: id}).then(res => {
+          console.log(res)
+        })
+      },
       handleCheckChange(data, checked, indeterminate) {
               console.log(data, checked, indeterminate);
             },
